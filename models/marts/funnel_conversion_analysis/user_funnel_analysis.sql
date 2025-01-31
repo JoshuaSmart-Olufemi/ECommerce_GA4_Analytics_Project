18dd4c7782baf54442c96a4552cb898a411bb587
Joshua Smart Olufemi <joshuasmartolufemi@gmail.com>
2025-01-31 13:25:59 +0000

    new models across all three marts

--- models/marts/funnel_conversion_analysis/user_funnel_analysis.sql
+++ models/marts/funnel_conversion_analysis/user_funnel_analysis.sql
@@ -1,31 +1,9 @@
-WITH raw_events AS (
+WITH event_steps AS (
+-- Assigning step numbers to key funnel events
+
     SELECT 
         *
-    FROM {{ ref('stg_event__json_extract') }}
-),
-
--- Assigning step numbers to key funnel events
-event_steps AS (
-    SELECT 
-        user_pseudo_id,
-        ga_session_id,
-        event_timestamp,
-        event_name,
-        CASE 
-            WHEN event_name = 'session_start' THEN 1
-            WHEN event_name = 'first_visit' THEN 2
-            WHEN event_name = 'page_view' THEN 3
-            WHEN event_name = 'view_search_results' THEN 4
-            WHEN event_name = 'view_item' THEN 5
-            WHEN event_name = 'view_promotion' THEN 6
-            WHEN event_name = 'select_promotion' THEN 7
-            WHEN event_name = 'click' THEN 8
-            WHEN event_name = 'add_shipping_info' THEN 9
-            WHEN event_name = 'add_payment_info' THEN 10
-            WHEN event_name = 'user_engagement' THEN 11
-            WHEN event_name = 'scroll' THEN 12
-        END AS funnel_step
-    FROM raw_events
+    FROM {{ ref('int_event_steps') }}
 ),
 
 -- Aggregating funnel data per session
--- models/marts/user_engagement/page_views_per_traffic_source.sql
+++ models/marts/user_engagement/page_views_per_traffic_source.sql
@@ -9,11 +9,12 @@ with base as (
 
     select
     traffic_source_name
+    , traffic_medium
     , count(1) as pg_views_per_traffic_source_count
     from base 
     where event_name = 'page_view'
-    group by 1
-    order by 2 desc
+    group by 1, 2
+    order by 3 desc
 
 )
 select * from page_views
